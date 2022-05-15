20207002655265405ddbfa760cc5ae195fc06426
maple <zhangyichi0826@gmail.com>
2022-05-15 10:38:26 +0800

    modify StaticValue.java

--- src/StaticValue.java
+++ src/StaticValue.java
@@ -1,6 +1,6 @@
 public class StaticValue {
     static double stageWidth = 800;
     static double stageHeight = 450;
-    static String userName = "zhangyichi";
+    static String userName;
     static String course;
 }
