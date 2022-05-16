14e9818b83cdacc5e53ffe7010d2ac5ed821fb2b
maple <zhangyichi0826@gmail.com>
2022-05-16 16:57:49 +0800

    add Edit and Delete function && add Menu function

--- src/Course.java
+++ src/Course.java
@@ -1,7 +1,12 @@
+import javafx.event.Event;
+import javafx.event.EventHandler;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
+import javafx.geometry.Side;
 import javafx.scene.Scene;
 import javafx.scene.control.*;
+import javafx.scene.input.MouseButton;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.*;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Line;
@@ -12,6 +17,7 @@ import javafx.stage.Modality;
 import javafx.stage.Stage;
 
 import java.io.File;
+import java.io.IOException;
 import java.util.Arrays;
 
 public class Course {
@@ -20,11 +26,10 @@ public class Course {
     Label courseIconLabel = new CourseIconLabel().getIconLabel();
     Label courseUserName = new CourseUserName().getUserName();
     Button courseExitButton = new CourseExitButton().getExit();
-    Button courseAddButton = new CourseAddButton().getAdd();
     HBox courseTopHBox = new CourseTopHBox(courseUserName,courseIconLabel,courseExitButton).gethBox();
     Line topLine = new Line(0,StaticValue.stageHeight / 7,
             StaticValue.stageWidth,StaticValue.stageHeight / 7);
-    VBox courseLeftVBox = new CourseLeftVBox().getvBox();
+    ScrollPane courseLeftScrollPane = new CourseLeftScrollPane().getPane();
 
     public Course(Stage stage) {
         this.stage = stage;
@@ -32,8 +37,7 @@ public class Course {
     }
     private void createCourse() {
         coursePane.setTop(courseTopHBox);
-        coursePane.setBottom(courseAddButton);
-        coursePane.setLeft(new ScrollPane(courseLeftVBox));
+        coursePane.setLeft(courseLeftScrollPane);
         coursePane.getChildren().add(topLine);
         stage.setScene(new Scene(coursePane,StaticValue.stageWidth,StaticValue.stageHeight));
         stage.setTitle("学习小帮手");
@@ -91,12 +95,11 @@ class CourseTopHBox {
     }
 }
 
-class CourseLeftVBox {
+class CourseLeftScrollPane {
+    private ScrollPane pane = new ScrollPane();
     private VBox vBox = new VBox();
-    VBox getvBox() {
-        return this.vBox;
-    }
-    CourseLeftVBox() {
+    private ContextMenu menu;
+    CourseLeftScrollPane() {
         vBox.setSpacing(StaticValue.stageHeight / 20);
         File f = new File("./data/" + StaticValue.userName);
         File[] tmp = f.listFiles();
@@ -108,23 +111,67 @@ class CourseLeftVBox {
         Arrays.sort(course);
         Button[] courseButton = new Button[course.length];
         for(int i = 0;i < course.length;i++) {
-            courseButton[i] = new Button(course[i]);
-            courseButton[i].setFont(Font.font("宋体",24));
+            courseButton[i] = new CourseButton(course[i]).getButton();
             vBox.getChildren().add(courseButton[i]);
         }
+
+        pane.setContent(vBox);
+
+        menu = new ContextMenu();
+        MenuItem itemAdd = new MenuItem("添加");
+        menu.getItems().add(itemAdd);
+        pane.setOnMouseClicked(new EventHandler<MouseEvent>() {
+            public void handle(MouseEvent event) {
+                if (event.getButton().equals(MouseButton.SECONDARY)) {
+                    menu.show(vBox, Side.TOP, event.getX(), event.getY());
+                }
+                else if (event.getButton().equals(MouseButton.PRIMARY)) {
+                    menu.hide();
+                }
+            }
+        });
+        itemAdd.setOnAction(e -> {
+            new CourseAlert("Add");
+        });
+    }
+
+    public ScrollPane getPane() {
+        return pane;
     }
 }
 
-class CourseAddButton {
-    private Button add = new Button("添加");
-    Button getAdd() {
-        return this.add;
-    }
-    CourseAddButton() {
-        add.setFont(Font.font("黑体", FontWeight.BOLD,18));
-        add.setOnAction(e -> {
-            new CourseAlert("Add");
+class CourseButton {
+    private String course;
+    private Button button;
+    private ContextMenu menu;
+    public CourseButton(String course) {
+        this.course = course;
+        button = new Button(course);
+        button.setFont(Font.font("宋体",24));
+        menu = new ContextMenu();
+        MenuItem itemDel = new MenuItem("删除");
+        MenuItem itemEdit = new MenuItem("编辑");
+        menu.getItems().addAll(itemDel,itemEdit);
+        button.setOnMouseClicked(new EventHandler<MouseEvent>() {
+            public void handle(MouseEvent event) {
+                if(event.getButton().equals(MouseButton.SECONDARY)) {
+                    menu.show(button, Side.TOP, event.getX(), event.getY());
+                }
+                else if(event.getButton().equals(MouseButton.PRIMARY)) {
+                    menu.hide();
+                }
+            }
         });
+        itemDel.setOnAction(e -> {
+            new CourseAlert("Delete",this.course);
+        });
+        itemEdit.setOnAction(e -> {
+            new CourseAlert("Edit",this.course);
+        });
+    }
+
+    public Button getButton() {
+        return button;
     }
 }
 
@@ -157,11 +204,19 @@ class CourseAlert {
             alert.setContentText("该课程已存在！");
             alert.show();
         }
-        else if(s.equals("Success")) {
+        else if(s.equals("SuccessAdd")) {
             Alert alert = new Alert(Alert.AlertType.INFORMATION);
             alert.setTitle("提示信息");
             alert.setHeaderText("");
             alert.setContentText("添加成功！");
+            alert.show();
+            new Course(Course.stage);
+        }
+        else if(s.equals("SuccessEdit")) {
+            Alert alert = new Alert(Alert.AlertType.INFORMATION);
+            alert.setTitle("提示信息");
+            alert.setHeaderText("");
+            alert.setContentText("更改成功！");
             alert.show();
             new Course(Course.stage);
         }
@@ -201,7 +256,67 @@ class CourseAlert {
                 new File("./data/" + StaticValue.userName + "/" + course + "/ddl").mkdir();
                 new File("./data/" + StaticValue.userName + "/" + course + "/资源").mkdir();
                 alert.close();
-                new CourseAlert("Success");
+                new CourseAlert("SuccessAdd");
+            });
+        }
+    }
+    CourseAlert(String s,String course) {
+        if(s.equals("Delete")) {
+            Alert alert = new Alert(Alert.AlertType.NONE);
+            ButtonType buttonYes = new ButtonType("确定");
+            ButtonType buttonNo = new ButtonType("取消");
+            alert.getButtonTypes().setAll(buttonYes, buttonNo);
+            alert.setTitle("提示信息");
+            alert.setHeaderText("");
+            alert.setContentText("确定要删除课程：" + course + " 吗？");
+            alert.initOwner(Course.stage);
+            alert.show();
+            alert.setOnCloseRequest(e -> {
+                ButtonType result = alert.getResult();
+                if (result != null && result.equals(buttonYes)) {
+                    try {
+                        System.out.println("./data/" + StaticValue.userName + "/" + course);
+                        StaticValue.deleteFile(new File("./data/" + StaticValue.userName + "/" + course));
+                    } catch (IOException ex) {
+                        ex.printStackTrace();
+                    }
+                    new Course(Course.stage);
+                } else {
+                    alert.close();
+                }
+            });
+        }
+        else if(s.equals("Edit")) {
+            Text text = new Text("请输入新课程名称：");
+            text.setFont(Font.font("宋体",18));
+            TextField textField = new TextField();
+            Button yes = new Button("确定");
+            Button no = new Button("取消");
+            HBox hBox1 = new HBox(text,textField);
+            HBox hBox2 = new HBox(yes,no);
+            hBox2.setAlignment(Pos.CENTER);
+            BorderPane borderPane = new BorderPane();
+            borderPane.setCenter(hBox1);
+            borderPane.setBottom(hBox2);
+            borderPane.setMargin(hBox1,new Insets(30));
+            Stage alert = new Stage();
+            alert.initModality(Modality.APPLICATION_MODAL);
+            alert.setScene(new Scene(borderPane,StaticValue.stageWidth * 2 / 5,StaticValue.stageHeight * 2 / 5));
+            alert.setTitle("提示信息");
+            alert.show();
+
+            no.setOnAction(e -> {
+                alert.close();
+            });
+
+            yes.setOnAction(e -> {
+                String newCourse = textField.getText();
+                File f1 = new File("./data/" + StaticValue.userName + "/" + course);
+                File f2 = new File("./data/" + StaticValue.userName + "/" + newCourse);
+                alert.close();
+                f1.renameTo(f2);
+                new CourseAlert("SuccessEdit");
+                new Course(Course.stage);
             });
         }
     }
--- src/StaticValue.java
+++ src/StaticValue.java
@@ -1,6 +1,24 @@
+import java.io.File;
+import java.io.IOException;
+
 public class StaticValue {
     static double stageWidth = 800;
     static double stageHeight = 450;
     static String userName;
     static String course;
+
+    public static void deleteFile(File file) throws IOException {
+        File[] files = file.listFiles();
+        if (files!=null){
+            for (int i = 0; i < files.length ; i++) {
+                if (files[i].isFile()){
+                    files[i].delete();
+                }else if (files[i].isDirectory()){
+                    deleteFile(files[i]);
+                }
+                files[i].delete();//删除子目录
+            }
+        }
+        file.delete();
+    }
 }
